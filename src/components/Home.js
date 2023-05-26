6ee542af8a28e3ccd8e1ffc2e039f2c3bb66ce53
Jay Liang <jayliang789@gmail.com>
2023-05-26 01:58:08 -0400

    changed title

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
     return(
         <div className='h-[1500px] overflow-y-auto'>
             <div className='relative flex w-full h-[900px] bg-gray-900 items-center justify-center flex-col'>
-                <div className='w-[250px] h-[250px] rounded-full min-w-[250px]'>
-                    <img className='w-[250px] h-[250px] rounded-full object-cover pointer-events-none select-none' src={pfp}></img>
+                <div className='w-[250px] h-[250px] rounded-full min-w-[250px] pointer-events-none select-none'>
+                    <img className='w-[250px] h-[250px] rounded-full object-cover' alt="myProfile" src={pfp}></img>
                 </div>
                 <div className='text-4xl text-cyan-600 mt-6' style={{fontFamily: 'cursive', transform: 'rotate(-5deg)'}}>
                     ―Jay Liang―
