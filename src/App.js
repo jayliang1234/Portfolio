23485f939eebcb028bd314f083c7f46669f3d461
Jay Liang <jayliang789@gmail.com>
2023-05-26 00:21:03 -0400

    .

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
+        <Route path="/" element={<Home />}/>
         <Route path="/Portfolio" element={<Home />} />
       </Routes>
     </Router>
