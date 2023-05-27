a4fbf6c21fb91f6e5b9b8df1a2ae072263082b56
Jay Liang <jayliang789@gmail.com>
2023-05-26 23:03:08 -0400

    changed font

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import pfp from '../assets/pfp.png'
 
+
 const Home = () => {
     return(
         <div className='h-[1500px] overflow-y-auto'>
-            <div className='relative flex w-full h-[900px] bg-gray-900 items-center justify-center flex-col'>
-                <div className='w-[250px] h-[250px] rounded-full min-w-[250px] pointer-events-none select-none'>
-                    <img className='w-[250px] h-[250px] rounded-full object-cover' alt="myProfile" src={pfp}></img>
+            <div className='relative flex w-full h-screen bg-gray-900 items-center justify-center flex-col'>
+                <div className='w-[200px] h-[200px] rounded-full min-w-[200px] pointer-events-none select-none'>
+                    <img className='w-[200px] h-[200px] rounded-full object-cover' alt="myProfile" src={pfp}></img>
                 </div>
-                <div className='text-4xl text-cyan-600 mt-6' style={{fontFamily: 'cursive', transform: 'rotate(-5deg)'}}>
-                    ―Jay Liang―
+                <div className='texting text-4xl text-cyan-600 mt-6' style={{ fontFamily: "'Kaushan Script', cursive", transform: 'rotate(-5deg)' }}>
+                    ― Jay Liang ―
                 </div>
             </div>
 
