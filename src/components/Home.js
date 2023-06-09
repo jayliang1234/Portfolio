6e1fa1124cb310b779afd8b01c5a92b73a3fe9bb
Jay Liang <jayliang789@gmail.com>
2023-06-09 19:58:36 -0400

    added skills and changed css for about me section to adjust to screen sizes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ import {FaGithubSquare} from 'react-icons/fa'
 
 const Home = () => {
     return(
-        <div className='h-[1500px]'>
-            <div className='relative flex w-full h-screen bg-gray-900 items-center justify-center flex-col'>
+        <div className=''>
+            <div className='relative flex w-full h-screen bg-neutral-950 items-center justify-center flex-col'>
                 <div className='w-[200px] rounded-full min-w-[200px] pointer-events-none select-none'>
                     <img className='w-full h-[200px] rounded-full object-cover' alt="myProfile" src={pfp}></img>
                 </div>
@@ -18,26 +18,37 @@ const Home = () => {
                     software engineer
                 </div>
             </div>
-            <div className='relative w-full h-[30%] bg-white' style={{fontFamily: "'Space Mono', Arial, serif"}}>
-                <ul>
-                    <li className='relative text-red-400 mt-32 text-5xl h-[20%] text-center'>ABOUT ME</li>
-                    <ul className='ml-20 mt-20'>
-                        <li className='inline-block font-bold'>Name: </li>
-                        <li className='inline-block ml-[70px] text-gray-400'>Jay Liang</li>
-                    </ul>
-                    <ul className='ml-20 mt-7'>
-                        <li className='inline-block font-bold'>Email: </li>
-                        <li className='inline-block ml-[60px] text-gray-400'>jay.liang74@myhunter.cuny.edu</li>
-                    </ul>
-                    <ul className='relative ml-[600px] bottom-[75px]'>
-                        <li className='text-4xl'>Hey, thanks for stopping by! </li>
-                        <span className='mt-7 block w-[800px]'>I am a recent graduate of Hunter College, where I earned a Bachelor of Arts degree in computer science. My aspiration is to leverage my skills and knowledge as a software engineer in a dynamic and challenging role within a renowned tech company.</span>
-                    </ul>
-                    <ul className='ml-[600px]'>
-                        <a href='https://www.linkedin.com/in/jay-liang-205420197/' className=' text-red-500 hover:text-blue-500 transition-colors duration-300'><BsLinkedin className='inline-block' size={40}></BsLinkedin></a>
-                        <a href='https://github.com/jayliang1234' className=' text-red-500 hover:text-blue-500 transition-colors duration-300'><FaGithubSquare className='inline-block ml-2' size={45}></FaGithubSquare></a>
-                    </ul>
-                </ul>
+            <div className='relative text-red-400 mt-[3%] text-5xl text-center mb-[3%] bg-white' style={{fontFamily: "'Space Mono', Arial, serif"}}>ABOUT ME</div>
+            <div className='flex flex-row flex-wrap relative w-full bg-white justify-evenly' style={{fontFamily: "'Space Mono', Arial, serif"}}>
+                    <div className='flex flex-col'>
+                        <div className='pb-10'>
+                            <text className='inline-block font-bold ml-10'>Name: </text>
+                            <text className='inline-block ml-[70px] text-gray-400'>Jay Liang</text>
+                        </div>
+                        <div className='pb-10'>
+                            <text className='inline-block font-bold ml-10'>Email: </text>
+                            <text className='inline-block ml-[60px] text-gray-400 pb-10'>jay.liang74@myhunter.cuny.edu</text>
+                            <div>
+                                <text className='text-4xl relative 2xl:left-[450px] 2xl:bottom-[130px] ml-10'>Hey, thanks for stopping by! </text>
+                            </div>
+                            <p className='relative mt-7 w-[50%] min-w-[500px] 2xl:left-[450px] 2xl:bottom-[130px] ml-10'>I am a recent graduate of Hunter College, where I earned a Bachelor of Arts degree in computer science. My aspiration is to leverage my skills and knowledge as a software engineer in a dynamic and challenging role within a renowned tech company.</p>
+                            <div className='relative 2xl:left-[450px] 2xl:bottom-[100px] ml-10 pb-10'>
+                                <a href='https://www.linkedin.com/in/jay-liang-205420197/' className='text-red-500 hover:text-blue-500 transition-colors duration-300'><BsLinkedin className='inline-block' size={40}></BsLinkedin></a>
+                                <a href='https://github.com/jayliang1234' className='text-red-500 hover:text-blue-500 transition-colors duration-300'><FaGithubSquare className='inline-block ml-2' size={45}></FaGithubSquare></a>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div className='relative flex justify-center text-5xl bg-neutral-950 pt-[3%] pb-[3%] text-white' style={{fontFamily: "'Space Mono', Arial, serif"}}>Skills</div>
+            <div className='flex relative w-full h-full bg-neutral-950 px-[10%] flex-wrap justify-evenly pb-[5%]'>
+                <div className='rounded-full min-h-[250px] min-w-[250px] flex items-center justify-center bg-slate-50 text-3xl border-8 hover:border-blue-600 transition-colors duration-1000'>C++</div>
+                <div className='rounded-full min-h-[250px] min-w-[250px] flex items-center justify-center bg-slate-50 text-3xl border-8 hover:border-blue-600 transition-colors duration-1000'>JavaScript</div>
+                <div className='rounded-full min-h-[250px] min-w-[250px] flex items-center justify-center bg-slate-50 text-3xl border-8 hover:border-blue-600 transition-colors duration-1000'>HTML</div>
+            </div>
+            <div className='flex relative w-full h-full bg-neutral-950 px-[10%] flex-wrap justify-evenly pb-[5%]'>
+                <div className='rounded-full min-h-[250px] min-w-[250px] flex items-center justify-center bg-slate-50 text-3xl border-8 hover:border-blue-600 transition-colors duration-1000'>CSS</div>
+                <div className='rounded-full min-h-[250px] min-w-[250px] flex items-center justify-center bg-slate-50 text-3xl border-8 hover:border-blue-600 transition-colors duration-1000'>Python</div>
+                <div className='rounded-full min-h-[250px] min-w-[250px] flex items-center justify-center bg-slate-50 text-3xl border-8 hover:border-blue-600 transition-colors duration-1000'>MySQL</div>
             </div>
 
         </div>
