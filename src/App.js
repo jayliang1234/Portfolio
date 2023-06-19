3f06b48b9499f6d91a8a728d17168f871a932519
Jay Liang <jayliang789@gmail.com>
2023-06-19 15:36:13 -0400

    trying to fix background not auto playing in background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React from 'react'
-import Navbar from './components/Navbar.js';
 import Home from './components/Home';
 function App() {
   return (
